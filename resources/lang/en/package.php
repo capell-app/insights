df7fd73fc030b1bbddf80971df547c18a5e4c149
capell-app <appcapello@gmail.com>
2026-05-02 13:45:33 +0000

    Tweaks

---
+++ resources/lang/en/package.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'description' => 'Adds first-party analytics, visitor journeys, click tracking, and consent management.',
+];
---
+++ tests/Pest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Capell\Analytics\Tests\AnalyticsTestCase;
+
+pest()->extend(AnalyticsTestCase::class)->in('Feature', 'Unit');
