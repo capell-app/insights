630155d8d7a7adcd143caafa2b56001091f90670
Ben <howdu@users.noreply.github.com>
2026-07-12 08:56:25 +0100

    fix: align packages with Capell v4 extension API

--- src/Manifest/InsightsModelsContribution.php
+++ src/Manifest/InsightsModelsContribution.php
@@ -10,6 +10,6 @@ final class InsightsModelsContribution implements ExtensionContribution
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^0.0';
+        return '^4.0';
     }
 }
--- src/Manifest/InsightsSettingsContribution.php
+++ src/Manifest/InsightsSettingsContribution.php
@@ -11,6 +11,6 @@ final class InsightsSettingsContribution implements ExtensionContribution, Regis
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^0.0';
+        return '^4.0';
     }
 }
