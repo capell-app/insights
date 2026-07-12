2c7a4438b05da92c1bedd570dc5b733214237cc1
Ben <howdu@users.noreply.github.com>
2026-07-12 07:44:22 +0100

    fix: align packages with 0.0 release line

--- src/Health/InsightsHealthCheck.php
+++ src/Health/InsightsHealthCheck.php
@@ -29,7 +29,7 @@ final class InsightsHealthCheck implements ChecksExtensionHealth
 
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 
     /**
